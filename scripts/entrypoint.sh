850ff3005724324d4d64600b9545161a3da0a584
Caio Schmidt <caiofonseca6@gmail.com>
2025-12-05 13:34:30 -0300

    entrypoint box86

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -7,5 +7,5 @@ fi
 echo "[ANIV] Checking for updates..."
 ./aniv-ds.sh validate anonymous
 cd aniv-ds
-./aniv_server.x86_64 -map aneurismiv -maxplayers $MAX_PLAYERS -timestamps 2>&1 | tee ./logs/server_$NOW.log 
+box86 ./aniv_server.x86_64 -map aneurismiv -maxplayers $MAX_PLAYERS -timestamps 2>&1 | tee ./logs/server_$NOW.log 
 # ./aniv_server.x86_64 -map nightmare -timestamps
